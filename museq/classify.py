333ce98c1c43277ef8abcd24fad8ae13287a28a9
Jafar Taghiyar <jtaghiyar@bccrc.ca>
2013-08-07 11:24:08 -0700

    removed commented lines

--- museq/classify.py
+++ museq/classify.py
@@ -1,4 +1,3 @@
-#import os
 import argparse
 from datetime import datetime
 import sys
@@ -6,6 +5,9 @@ from warnings import warn
 
 mutationSeq_version="3.1.1"
 
+#==============================================================================
+# making a UI 
+#==============================================================================
 parser = argparse.ArgumentParser(description='''classify a dataset''')
 parser.add_argument("samples", nargs='*', help='''
                     A list of colon delimited sample names; normal:normal.bam
@@ -28,7 +30,7 @@ parser.add_argument("-c", "--config", default=None,
 args = parser.parse_args()
 
 #==============================================================================
-# check the input first
+# check the input 
 #==============================================================================
 if len(args.samples) != 4:
     print >> sys.stderr, "bad input, should follow: 'classify.py normal:<normal.bam> \
@@ -143,9 +145,8 @@ def extractFeature(tumour_data, normal_data, ref_data):
     features_tmp.append(n.xentropy(n_counts, t_counts))
     return features_tmp    
 
-
 #==============================================================================
-# main body
+# start of the main body
 #==============================================================================
 print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " mutationSeq_" + mutationSeq_version + " started"
 deep_flag = 0
@@ -172,78 +173,21 @@ model = RandomForestClassifier(random_state=0, n_estimators=1000, n_jobs=-1, com
 print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " model fitting started"
 model.fit(train, labels)
 print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " done model fitting"
-#==============================================================================
-# check npz in the input
-#==============================================================================
-#if not args.normalized:
-#    feature_set = features.feature_set
-#    coverage_features = features.coverage_features
-#    extra_features = (("xentropy", 0), ("SENTINEL", 0))
-#    version = features.version
-#    c = (float(30), float(30), float(70), float(0))
-#        
-#else:
-#    feature_set = Nfeatures.feature_set
-#    coverage_features = Nfeatures.coverage_features
-#    extra_features = (("xentropy", 0), ("SENTINEL", 0))
-#    version = Nfeatures.version
-#    c = (float(10000), float(10000), float(70), float(0))
 
-#if "npz" in samples:
-#    try:
-#        npz = numpy.load(samples["npz"])
-#    except:
-#        print >> sys.stderr, "\tCould not import npz feature vector"
-#        sys.exit(1)
-#    coords = npz["arr_1"]
-#    batch = npz["arr_0"]
-#    strings = npz["arr_2"]
-#    b = []
-#    c = []
-#    s = []
-#    for coord, l, string in zip(coords, batch, strings):
-#        if numpy.isnan(l).any() or numpy.isinf(l).any():
-#            print >> sys.stderr, "\tnan/inf value"
-#            continue
-#        b.append(l)
-#        c.append(coord)
-#        s.append(string)
-#    batch = numpy.array(b)
-#    coords = c
-#    strings = s
-#
-#    for coord, result, string in zip(coords, model.predict_proba(batch), strings):
-#        if result[1] >= args.threshold:
-#            print >> out, string[0], int(coord[0]), string[1], string[2], \
-#            int(coord[3]), string[3], int(coord[5]), string[4], int(coord[8]), \
-#            string[5], int(coord[10]), result[1], int(coord[-1])
-#    sys.exit(0)
-    
-#==============================================================================
 # if npz["arr_0"] != version:
 #     print >> sys.stderr, "\tmismatched feature set versions:",
 #     print >> sys.stderr, "\t" + str(npz["arr_0"]), "and", str(version)
 #     out.close()
 #     sys.exit(1)
-#==============================================================================
-
-
-#die = False
-#if args.export:
-#    try:
-#        print >> sys.stderr, "\ttrying", args.export + ".npz"
-#        x = numpy.load(args.export + ".npz")
-#        die = True
-#    except:
-#        print >> sys.stderr, "\tcase not run"
+#if args.verbose:
+#    print >> sys.stderr, "\tunsorted feature names"
+#    for importance, _feature in zip(model.feature_importances_, feature_set + coverage_features + extra_features):
+#        print >> sys.stderr, _feature[0], importance
 #
-#if die:
-#    print >> sys.stderr, "\tthis case has already been run", args.export + ".npz"
-#    sys.exit(1)
-#feature_set = features.feature_set
-#coverage_features = features.coverage_features
-#extra_features = (("xentropy", 0), ("SENTINEL", 0))
-##
+#    print >> sys.stderr, "\tsorted by importance:"
+#    for importance, _feature in sorted(zip(model.feature_importances_, feature_set + coverage_features + extra_features)):
+#        print >> sys.stderr, _feature[0], importance
+
 #==============================================================================
 # read in bam files
 #==============================================================================
@@ -263,17 +207,8 @@ except:
     print >> sys.stderr, "\tFailed to load reference"
     sys.exit(1)
 
-#if args.verbose:
-#    print >> sys.stderr, "\tunsorted feature names"
-#    for importance, _feature in zip(model.feature_importances_, feature_set + coverage_features + extra_features):
-#        print >> sys.stderr, _feature[0], importance
-#
-#    print >> sys.stderr, "\tsorted by importance:"
-#    for importance, _feature in sorted(zip(model.feature_importances_, feature_set + coverage_features + extra_features)):
-#        print >> sys.stderr, _feature[0], importance
-
 #==============================================================================
-# parse the positions or the file of list of positions
+# parse the positions or the file of list of positions to get targets
 #==============================================================================
 targets = None
 l_pos = None
@@ -296,9 +231,9 @@ elif args.positions_file:
 
 else:
     targets = set(n.targets) & set(t.targets)
-
+    
 #==============================================================================
-# Add VCF format meta-information from metadata.cfg file to the output file
+# add VCF format meta-information from metadata.cfg file to the output file
 #==============================================================================
 if args.config:
     try:
@@ -319,11 +254,6 @@ if args.config:
 #==============================================================================
 # run for each chromosome/position
 #==============================================================================
-#coverage_data = (30, 30, int(args.purity), 1)
-#total_batch = []
-#total_coords = []
-#total_strings = []
-
 for chrom in target_positions.keys(): # each key is a chromosomes
     for pos in range(len(target_positions[chrom])):
         l_pos = target_positions[chrom][pos][0]
@@ -349,7 +279,6 @@ for chrom in target_positions.keys(): # each key is a chromosomes
         except:
             message = "\treference does not have chromosome " + chrom
             warn(message)
-#            print >> sys.stderr, 
             continue
         print >> sys.stderr, "\treading tumour data"
         if l_pos is None:
@@ -363,7 +292,7 @@ for chrom in target_positions.keys(): # each key is a chromosomes
             position = tumour_data[0]
             ref_data = f.vector(int(position))
             
-            # Get tri-nucleotide context, seems not really an efficient way, but tested it is indeed readlly fast
+            ## get tri-nucleotide context, seems not really an efficient way, but tested it is indeed readlly fast
             if tumour_data[5][0] - tumour_data[ref_data[0] + 1][0] > 2 or args.all == "yes":
                 try:
                     pre_refBase = f.vector(int(position) - 1)[0]
@@ -402,17 +331,6 @@ for chrom in target_positions.keys(): # each key is a chromosomes
 #==============================================================================
 #             feature extraction
 #==============================================================================
-#            features = []
-#            for _, feature in feature_set:
-#                features.append(feature(tumour_data, normal_data, ref_data))
-#    
-#            for _, feature in coverage_features:
-#                features.append(feature(tumour_data, normal_data, coverage_data))
-#            n_counts = (normal_data[1][1], normal_data[2][1], normal_data[3][1],
-#                        normal_data[4][1], normal_data[5][1])
-#            t_counts = (tumour_data[1][1], tumour_data[2][1], tumour_data[3][1],
-#                        tumour_data[4][1], tumour_data[5][1])
-#            features.append(n.xentropy(n_counts, t_counts))
             features_tmp = extractFeature(tumour_data, normal_data, ref_data)
             batch.append(features_tmp)
             coords.append((position, ref_data[0], normal_data[6], normal_data[normal_data[6] + 1][0],
@@ -422,71 +340,38 @@ for chrom in target_positions.keys(): # each key is a chromosomes
             strings.append((chrom, bases[ref_data[0]], bases[normal_data[6]], bases[normal_data[7]], \
             bases[tumour_data[6]], bases[tumour_data[7]], tc))       
     
-            ## Find the ALT 
+            ## find the ALT 
             if bases[ref_data[0]]==bases[tumour_data[6]]:
                 alt = tumour_data[7]
             else:
                 alt = tumour_data[6]        
     
-            ## Generate the values of info fields in the vcf output
+            ## generate the values of info fields in the vcf output
             if alt != ref_data[0]:
                 info_strs.append((alt, int(tumour_data[ref_data[0] + 1][0]), int(tumour_data[alt + 1][0]), \
                 int(normal_data[ref_data[0] + 1][0]), int(normal_data[alt + 1][0])))
-            else: # to take care of the non-somatic positions
+            else: # take care of the non-somatic positions
                 info_strs.append((alt, int(tumour_data[ref_data[0] + 1][0]), 0, int(normal_data[ref_data[0] + 1][0]), 0))
     
             if len(positions) == 0:
                 break
-#        if args.export:
-#            total_batch += batch
-#            total_coords += coords
-#            total_strings += strings
         batch = numpy.array(batch)
     
-    #==============================================================================
-    # remove nan/inf values
-    #==============================================================================
+#==============================================================================
+#       remove nan/inf values
+#==============================================================================
         print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " removing potential nan/inf values"
         batch = removeNanInf(coords, batch, strings, info_strs)
-        
-#        for coord, l, string, info in zip(coords, batch, strings, info_strs):
-#            if numpy.isnan(l).any() or numpy.isinf(l).any():
-#                print >> sys.stderr, "\tnan/inf value at position "+ str(chrom) + ":" +  str(coord[0])
-#                continue
-#            b.append(l)
-#            c.append(coord)
-#            s.append(string)
-#            i.append(info)
-#        batch = numpy.array(b)
-#        coords = c
-#        strings = s
-#        info_strs = i
     
-    #==============================================================================
-    # filter and print the results to out
-    #==============================================================================
-        print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " filtering and printing results"
-#        for coord, result, string, info in zip(coords, model.predict_proba(batch), strings, info_strs):
-#            if result[1] >= args.threshold:
-#                try:
-#                    phred_qual = -10 * log10(1 - result[1])            
-#                except:
-#                    phred_qual = 99
-#                info_str = "PR=" + "%.3f" % result[1] + ";TR=" + str(info[1]) + ";TA=" + str(info[2]) + ";NR=" + str(info[3]) + ";NA=" + str(info[4]) + ";TC=" + string[6]
-#                print >> out, str(string[0]) + "\t" + str(coord[0]) + "\t" + "." + "\t" + string[1] + "\t" + bases[info[0]] + "\t"+ "%.2f" % phred_qual + "\t" + "PASS" + "\t" + info_str
-#            elif args.all == "yes":
-#                phred_qual = 0
-#                info_str = "PR=" + "%.3f" % result[1] + ";TR=" + str(info[1]) + ";TA=" + str(info[2]) + ";NR=" + str(info[3]) + ";NA=" + str(info[4]) + ";TC=" + string[6]
-#                print >> out, str(string[0]) + "\t" + str(coord[0]) + "\t" + "." + "\t" + string[1] + "\t" + bases[info[0]] + "\t"+ "%.2f" % phred_qual + "\t" + "FAIL" + "\t" + info_str
-        
+#==============================================================================
+#       filter and print the results to out
+#==============================================================================
+        print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " filtering and printing results"      
         filterAndPrintResult(coords, batch, strings, info_strs)
     print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " done printing"        
-    #    print >> sys.stderr, "***No position has been nominated (does not satisfy initial criteria for Somatic calls )"
-#    total_batch = numpy.array(total_batch)
-##==============================================================================
-## Export the batch
-##==============================================================================
-#if args.export:
-#    numpy.savez(args.export, total_batch, total_coords, total_strings)
-#print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " done."
+#        print >> sys.stderr, "***No position has been nominated (does not satisfy initial criteria for Somatic calls )"
+#==============================================================================
+# end of the main body
+#==============================================================================
+print >> sys.stderr, datetime.now().strftime("%H:%M:%S") + " done."
     
