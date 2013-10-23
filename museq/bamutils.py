3c9f796e2c8e33551f24ea76ec11e7ac1158cc81
Jafar Taghiyar <jtaghiyar@bccrc.ca>
2013-10-23 12:21:01 -0700

    removed Bam class definition from bamutils.py and put it in pybamapi.py

--- museq/bamutils.py
+++ museq/bamutils.py
@@ -4,7 +4,6 @@ Created on Wed Sep 18 11:22:08 2013
 
 @author: jtaghiyar
 """
-import newpybam as np # new pybam
 import sys
 import numpy
 import newfeatures
@@ -16,139 +15,9 @@ from string import Template
 from datetime import datetime
 
 mutationSeq_version = "4.0.0"
+DEBUG = True
 
-class Bam:
-    def __init__(self, **kwargs):       
-        self.t_bam = kwargs.get("tumour")
-        self.n_bam = kwargs.get("normal")
-        self.ref   = kwargs.get("reference") 
-        self.rmdup = kwargs.get("rmdup")
-        self.base = {'A':0, 'C':1, 'G':2, 'T':3, 'N':4}
-        
-        ## check if duplicates need to be removed
-        if  self.rmdup is None:
-            self.rmdup = True
-            
-        ## make a pileup for normal bam
-        if  self.n_bam is not None:
-            self.n_pileup = np.pileup()
-            self.n_pileup.open(self.n_bam)
-    
-        ## make a pileup for tumour bam
-        if  self.t_bam is not None:
-            self.t_pileup = np.pileup() 
-            self.t_pileup.open(self.t_bam)
-        
-        if  self.ref is not None:
-            self.__load_reference()
-        
-    def __load_reference(self):
-        ## make a fasta object to laod reference
-        self.fasta = np.fasta() 
-        self.fasta.open(self.ref) 
-
-    def get_normal_refnames(self):
-        """ get the list of chromosome names and their corresponding IDs as a dictionary"""
-        
-        return dict(self.n_pileup.refnames)
-    
-    def get_tumour_refnames(self):
-        """ get the list of chromosome names and their corresponding IDs as a dictionary"""
-    
-        return dict(self.t_pileup.refnames)
-    
-    def get_normal_chromosome_id(self, chromosome_name):
-        rn = self.get_normal_refnames()
-        return rn.get(chromosome_name)
-    
-    def get_tumour_chromosome_id(self, chromosome_name):
-        rn = self.get_tumour_refnames()
-        return rn.get(chromosome_name)
-    
-    def get_normal_chromosome_name(self, chromosome_id):
-        rn = self.get_normal_refnames()
-        for k in rn.keys():
-            if rn[k] == chromosome_id:
-                return k
-        return None
-        
-    def get_tumour_chromosome_name(self, chromosome_id):
-        rn = self.get_tumour_refnames()
-        for k in rn.keys():
-            if rn[k] == chromosome_id:
-                return k
-        return None
-        
-    ##TODO: change the interface to chromosome name instead of chromosome_id for FASTA object
-    ## Maybe it is not a good idea since the tuples have chromosome_id and converting them to chromosome names
-    ## require extra function call to get_..._chromosome_name() which is a performance over head
-    def get_reference_base(self, chromosome_id, position, index=False):
-        b = self.fasta.get_base(chromosome_id, int(position))
-        
-        if index:
-            return self.base[b]
-        return b
-        
-    def get_reference_sequence(self, chromosome_id, position, windowLength=500):
-        return self.fasta.get_sequence(chromosome_id, position, windowLength)
-        
-    def get_reference_sequence_bybase(self, chromosome_id, position, windowLength=500):
-        return self.fasta.get_sequence_base(chromosome_id, position, windowLength)
-    
-    def get_trinucleotide_context(self, chromosome_id, position):
-        tc = []
-        for i in range(-1,2):
-            tc.append(self.get_reference_base(chromosome_id, position+i))
-        return ''.join(tc)
-
-    def get_reference_tuple(self, chromosome_id, position):
-        temp_tuple = self.fasta.get_tuple(chromosome_id, int(position))
-        refbase = temp_tuple[0]
-
-        # index refbase
-        refbase = self.base[refbase]
-
-        ## replace the base with its index (required for feature extraction)
-        temp_tuple = (refbase, temp_tuple[1], temp_tuple[2], temp_tuple[3], temp_tuple[4])
-        return temp_tuple
-            
-    def get_normal_tuple(self, chromosome, position):
-        self.n_pileup.set_region(chromosome, position, position)
-        return self.n_pileup.get_tuple()
-    
-    def get_tumour_tuple(self, chromosome, position):
-        self.t_pileup.set_region(chromosome, position, position)
-        return self.t_pileup.get_tuple()
-    
-    def get_normal_tuples(self, target_positions):
-        for tp in target_positions: 
-            if tp[1] is None:
-                self.n_pileup.set_region(tp[0])
-            else:
-                self.n_pileup.set_region(tp[0], tp[1], tp[2])
-
-            while True:
-                tt = self.n_pileup.get_tuple() 
-                if tt is None:
-                    break
-                else:
-                    yield tt
-            
-    def get_tumour_tuples(self, target_positions):
-        for tp in target_positions: 
-            if tp[1] is None:
-                self.t_pileup.set_region(tp[0])
-            else:
-                self.t_pileup.set_region(tp[0], tp[1], tp[2])
-
-            while True:
-                tt = self.t_pileup.get_tuple() 
-                if tt is None:
-                    break
-                else:
-                    yield tt
-            
-class BamUtils:
+class BamHelper:
     def __init__(self, bam, args):
         self.samples = {}
         for s in args.samples:
@@ -159,15 +28,23 @@ class BamUtils:
         if "normal" not in self.samples:
             single_flag = True
             single_type = "t"
-            
         elif "tumour" not in self.samples:
             single_flag = True
             single_type = "n"
-        
         else:
             single_flag = False
             single_type = None
-
+         
+        ## set the right feature set
+        if args.single:
+#            self.features = newfeatures_single
+            pass
+        elif args.deep:
+#            self.features = newfeatures_deep
+            pass
+        else:
+            self.features = newfeatures
+            
         self.outstr_buffer = []        
         self.flags = Flags._make([single_flag, single_type])
         self.base = ['A', 'C', 'G', 'T', 'N']
@@ -265,24 +142,33 @@ class BamUtils:
         features_buffer = []
         
         ## TODO: remove this, write tuples in a file
-        tuple_file = open("tuples.f", 'w')
+        if DEBUG:        
+            tuple_file = open("tuples.f", 'w')
+            print >> tuple_file, "Tumour tuples"
+            
         for tt in tumour_tuples:
             ## ignore tumour tuples with no/few variants compared to reference            
             refbase = self.bam.get_reference_base(tt[-1], tt[0], index=True)
             if tt[5][0] - tt[refbase + 1][0] < 3:
                 continue
             
+            ##TODO: remove this            
+            if DEBUG:
+                print >> tuple_file, tt
+                
             ## buffer tumour tuple to campare against normal tuples
             tuples_buffer.append(tt)
             
-            ##TODO: remove this
-            print >> tuple_file, tt
-
         ## return if all tuples were filterd
         if len(tuples_buffer) == 0:
             return []
         
         tt = tuples_buffer.popleft()
+        
+        ##TODO: remove this
+        if DEBUG:
+            print >> tuple_file, "Normal tuples"
+            
         for nt in normal_tuples:
             ## find positions where tuples for both tumour and normal exist
             while tt[0] < nt[0]:
@@ -292,13 +178,17 @@ class BamUtils:
 
             if tt[0] != nt[0]:
                 continue
-
+            
+            ##TODO: remvoe this:
+            if DEBUG:
+                print >> tuple_file, nt
+                
             ## extract reference tuples            
             rt = self.bam.get_reference_tuple(tt[-1], tt[0])
 
             ## calculate features      
-            feature_set = newfeatures.Features(tt, nt, rt)
-            tf = feature_set.get_features()
+            feature_set = self.features.Features(tt, nt, rt)
+            tf = feature_set.get()
             features_buffer.append(tf)
             
             ## generate output string and buffer it
@@ -309,7 +199,8 @@ class BamUtils:
                 break
         
         ##TODO: remove this
-        tuple_file.close()
+        if DEBUG:
+            tuple_file.close()
         
         ## make a numpy array required as an input to the random forest predictor
         features_buffer = numpy.array(features_buffer)
@@ -407,7 +298,12 @@ class BamUtils:
             
             print >> out, "\t".join(outstr)
     
-
+    def export_features(self, features):
+#        names = 
+        
+        with open(self.args.export, 'w') as export_file:
+            for f in features:
+                print >> export_file, f
 
 
 
